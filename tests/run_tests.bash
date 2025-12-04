49555df3416a2baa5efa5aceb2f65606cb9216e6
JoaquinVeraOrtega <j.veraortega99@gmail.com>
2025-12-04 01:48:39 -0300

    Fix run tests script closing terminal

--- tests/run_tests.bash
+++ tests/run_tests.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 if [ -z "$VIRTUAL_ENV" ]; then
     echo "Please activate your virtual environment before running tests."
-    exit 1
 fi
+if [ -n "$VIRTUAL_ENV" ]; then
 pytest test_unit.py
 pytest test_integration.py
+fi
